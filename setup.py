f203aa0172a72a830a39b2b661bef9f9d8daa892
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2024-01-08 13:54:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="MyList",
+    name="mylinkedlist",
     version="0.0.1",
     author="Opeoluwa Adeyeri",
     author_email="adeyeriopeoluwa05@gmail.com",
