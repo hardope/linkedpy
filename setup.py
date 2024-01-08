9091a60d354ee8cad9d4ac177eeb45e87e71be11
hardope <adeyeriopeoluwa05@gmail.com>
2024-01-08 13:40:24 +0000

    fix

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="linkedpy",
-    version="0.0.1",
+    version="0.0.2",
     author="Opeoluwa Adeyeri",
     author_email="adeyeriopeoluwa05@gmail.com",
     url="https://github.com/hardope/MyList",
     description="Python implementation of a default List using linked list",
     long_description=long_description,  # Use the content from README.md or another file
     long_description_content_type="text/markdown",  # Specify the type of content
-    packages=find_packages(),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
